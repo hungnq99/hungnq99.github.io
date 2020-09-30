7e43e8db0642596706ecfc90f3f710b2c323885c
Hung Nguyen <60608285+hungnq99@users.noreply.github.com>
2020-09-30 22:45:42 +0700

    complete about us

--- resources/js/script.js
+++ resources/js/script.js
@@ -16,7 +16,7 @@ $(document).ready(
 
 $(document).ready(
     function() {
-        $('.story').waypoint(
+        $('#story__item-1').waypoint(
             function(direction) {
                 if(direction =="down"){
                     $('#left').addClass('story__content');
@@ -32,4 +32,54 @@ $(document).ready(
             }
         )
     }
+)
+$(document).ready(
+    function() {
+        $('#story__item-2').waypoint(
+            function(direction) {
+                if(direction =="down"){
+                    $('#right-1').addClass('animationRight');
+                    $('#right-1').addClass('story__img');
+                    $('#right-1').css("opacity","1");
+                    $('#left-1').addClass('animationLeft');
+                    $('#left-1').addClass('story__content');
+                    $('#left-1').css("opacity","1");
+                }
+                
+            },{
+                offset: '600px'
+            }
+        )
+    }
+)
+
+// $(document).ready(
+//     function() {
+//         $('.story-background').waypoint(
+//             function(direction) {
+//                 if(direction =="down"){
+//                     $('.story-background__caption').addClass('bound');
+//                 }
+                
+//             },{
+//                 offset: '600px'
+//             }
+//         )
+//     }
+// )
+
+$(document).ready(
+    function() {
+        $('.background').waypoint(
+            function(direction) {
+                if(direction =="down") {
+                    $('nav').addClass('sticky');
+                }else {
+                    $('nav').removeClass('sticky');
+                }
+            }, {
+                offset : '30px'
+            }
+        )
+    }   
 )
